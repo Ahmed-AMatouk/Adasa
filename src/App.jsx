ca936ceece610352810971a22805b79129f61013
Ahmed Ali <163016715+Ahmed-AMatouk@users.noreply.github.com>
2026-02-13 09:42:50 +0200

    Change router from HashRouter to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { RouterProvider , createHashRouter} from 'react-router-dom';
+import { RouterProvider , createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
 import Blog from './components/Blog/Blog';
@@ -8,7 +8,7 @@ import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
 import BlogLayout from './components/blogLayout/BlogLayout';
 
-let router  = createHashRouter([
+let router  = createBrowserRouter([
   {path:"",
    element: <Layout/> ,
    children: [
